a4e1f429e5ab1ed1174076a584c567fca752f778
Costea-B <155655464+Costea-B@users.noreply.github.com>
2025-10-07 16:37:41 +0300

    init

---
+++ lib/pages/Produc_Card/Mock_Data/mock_data_recommendation.dart
@@ -0,0 +1,26 @@
+import '../../../models/product.dart';
+
+final List<Product> mockProducts = [
+  Product(
+    name: "Evening Dress",
+    brand: "Dorothy Perkins",
+    price: 125,
+    oldPrice: 155,
+    imageUrl: "assets/evening_dress.png",
+    discount: 20,
+  ),
+  Product(
+    name: "Sport Dress",
+    brand: "Silly",
+    price: 195,
+    oldPrice: 225,
+    imageUrl: "assets/new2.png",
+    discount: 15,
+  ),
+  Product(
+    name: "Short Dress",
+    brand: "H&M",
+    price: 19.99,
+    imageUrl: "assets/new2.png",
+  ),
+];
